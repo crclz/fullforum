b0d6cf2ab44ddd3f740b12ab7bc23d66d772e825
crclz <crucialize@foxmail.com>
2020-07-21 11:49:47 +0800

    init

---
+++ src/test/java/crclz/fullforum/FullforumApplicationTests.java
@@ -0,0 +1,13 @@
+package crclz.fullforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
