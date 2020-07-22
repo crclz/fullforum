8fe533c36a7500afeb94cbebeb4e159a35573164
crclz <crucialize@foxmail.com>
2020-07-22 19:41:19 +0800

    make FakeAuth request scoped

--- src/test/java/crclz/fullforum/TestServiceConfiguration.java
+++ src/test/java/crclz/fullforum/TestServiceConfiguration.java
@@ -6,12 +6,14 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.web.context.annotation.RequestScope;
 
 @TestConfiguration
 public class TestServiceConfiguration {
 
     @Bean
     @Primary// Configuration一直是生效的，本类只是“重写”了某些东西，所以primary
+    @RequestScope
     public FakeAuth constructFakeAuth() {
         return new FakeAuth();
     }
